51d7cdc45dc293e6b7d040c950a4f1feff7e71f5
biospank <fabio.petrucci@gmail.com>
2017-03-01 12:45:15 +0100

    remove comments

--- apps/sso/web/controllers/doc_controller.ex
+++ apps/sso/web/controllers/doc_controller.ex
@@ -5,6 +5,5 @@ defmodule Sso.DocController do
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
     |> Plug.Conn.send_file(200, Application.app_dir(:sso, "priv/static/index.html"))
-    # |> Plug.Conn.send_file(200, "./apps/sso/priv/static/index.html")
   end
 end
--- apps/sso_web/web/views/page_view.ex
+++ apps/sso_web/web/views/page_view.ex
@@ -2,11 +2,8 @@ defmodule SsoWeb.PageView do
   use SsoWeb.Web, :view
 
   def sso_web_static_path(conn, path) do
-    IO.puts page_path(conn, :index)
-    
     case Mix.env do
       :prod ->
-        # page_path(conn, :index) <> static_path(conn, path)
         page_path(conn, :index) <> static_path(conn, path)
       _ ->
         static_path(conn, path)
