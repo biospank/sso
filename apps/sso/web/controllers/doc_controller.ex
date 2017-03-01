979db22a90c4b0aaf5b7066b07df8b73bc2fa19a
biospank <fabio.petrucci@gmail.com>
2017-03-01 14:43:03 +0100

    note

--- apps/sso/web/controllers/doc_controller.ex
+++ apps/sso/web/controllers/doc_controller.ex
@@ -4,6 +4,6 @@ defmodule Sso.DocController do
   def index(conn, _params) do
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> Plug.Conn.send_file(200, Application.app_dir(:sso, "priv/static/index.html"))
+    |> Plug.Conn.send_file(200, Application.app_dir(:sso, "priv/static/index.html")) # or :code.priv_dir(my_app)
   end
 end
