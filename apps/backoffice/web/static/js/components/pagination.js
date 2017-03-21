2138367f888990374ebb5455f3abc2200b0839ff
biospank <fabio.petrucci@gmail.com>
2017-03-21 12:06:28 +0100

    remove log and comments on pagination

--- apps/backoffice/web/static/js/components/pagination.js
+++ apps/backoffice/web/static/js/components/pagination.js
@@ -59,78 +59,12 @@ const Pagination = () => {
             m('i.right teal chevron icon')
           ])
         ));
-        // return m("nav", { class: "text-right" }, [
-        //   m("ul", { class: "pagination" }, [].concat(
-        //     m("li", {}, [
-        //       m("a", {
-        //         href: "#",
-        //         onclick: (event) => {
-        //           event.preventDefault();
-        //           if(this.prevAvailable())
-        //             this.pageInfo.xhr(this.paramsFor(this.pageInfo.pageNumber - 1));
-        //           else
-        //             void(0);
-        //         },
-        //         "aria-label": "Precedente",
-        //         class: this.prevAvailable() ? "active" : "disabled"
-        //       }, [
-        //         m("span", { "aria-hidden": "true" }, m.trust("&laquo;"))
-        //       ])
-        //     ])).concat(
-        //       (Array.apply(null, Array(this.pageInfo.totalPages))).map((_, idx) => {
-        //         return m(new PaginationLink(), {
-        //           action: (event) => {
-        //             event.preventDefault();
-        //             this.pageInfo.xhr(this.paramsFor(idx + 1));
-        //           },
-        //           idx: (idx + 1),
-        //           currentPage: this.pageInfo.pageNumber
-        //         });
-        //       })
-        //     ).concat(
-        //     m("li", {}, [
-        //       m("a", {
-        //         href: "#",
-        //         onclick: (event) => {
-        //           event.preventDefault();
-        //           if(this.nextAvailable())
-        //             this.pageInfo.xhr(this.paramsFor(this.pageInfo.pageNumber + 1));
-        //           else
-        //             void(0);
-        //         },
-        //         "aria-label": "Successiva",
-        //         class: this.nextAvailable() ? "active" : "disabled"
-        //       }, [
-        //         m("span", { "aria-hidden": "true" }, m.trust("&raquo;"))
-        //       ])
-        //     ])
-        //   ))
-        // ])
       } else {
         return m('');
       }
     }
   };
 }
-
-// const LeftChevronLink = () => {
-//   return {
-//     view({attrs}) {
-//       return m('a.icon item', {
-//         href: '#',
-//         onclick: (event) => {
-//           event.preventDefault();
-//           if(this.prevAvailable())
-//             this.pageInfo.xhr(this.paramsFor(this.pageInfo.pageNumber - 1));
-//           else
-//             void(0);
-//         },
-//         class: this.prevAvailable() ? 'active' : 'disabled'
-//       }, [
-//         m('i.left chevron icon')
-//       ])
-//   };
-// }
 
 const PaginationLink = () => {
   return {
