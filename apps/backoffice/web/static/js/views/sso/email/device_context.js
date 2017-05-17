8537040b33a75bb3a9063ca5ee4f2f501e616f48
biospank <fabio.petrucci@gmail.com>
2017-05-17 16:05:08 +0200

    fix tab navigation

--- apps/backoffice/web/static/js/views/sso/email/device_context.js
+++ apps/backoffice/web/static/js/views/sso/email/device_context.js
@@ -4,30 +4,37 @@ import aceEditor from '../../../components/ace_editor';
 const deviceContextView = {
   view(vnode) {
     return [
-      m(".ui pointing secondary device-context menu", {
+      m(".ui pointing secondary web menu", {
         oncreate(_) {
-          $('.device-context.menu .item').tab();
+          $('.web.menu .item').tab();
         }
       }, [
         m("a.item active", {"data-tab": "web"}, "Web"),
         m("a.item", {"data-tab": "mobile"}, "Mobile")
       ]),
       m(".ui tab segment active", {"data-tab": "web"}, [
-        m("h4.ui header", "Template email SSO per applicazioni web"),
-        m(".ui template styled accordion", {
-          oncreate({dom}) {
-            $(dom).accordion();
+        m(".ui pointing secondary template menu", {
+          oncreate(_) {
+            $('div[data-tab="web"] .template.menu .item').tab();
           }
         }, [
-          m(".active title", [
-            m("i.dropdown icon"),
-            "Registrazione"
+          m("a.item active", {"data-tab": "web-registration"}, "Registrazione"),
+          m("a.item", {"data-tab": "web-confirm-registration"}, "Conferma Registrazione")
+        ]),
+        m(".ui tab segment active", {"data-tab": "web-registration"}, [
+          m(".ui pointing secondary registration-format menu", {
+            oncreate(_) {
+              $('div[data-tab="web"] .registration-format.menu .item').tab();
+            }
+          }, [
+            m("a.item active", {"data-tab": "web-html-registration"}, "HTML"),
+            m("a.item", {"data-tab": "web-text-registration"}, "TESTO")
           ]),
-          m(".active content", [
+          m(".ui tab segment active", {"data-tab": "web-html-registration"}, [
             m("form.ui form", [
               m(".field", [
                 m("label", "Oggetto"),
-                m("input", {type: "text", name: "web-registration-subject", placeholder: "Oggetto della mail"})
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
               ]),
               m(".field", [
                 m("label", "Body (formato html)"),
@@ -36,8 +43,25 @@ const deviceContextView = {
                   fontSize: "16px",
                   theme: "ace/theme/twilight",
                   mode: "ace/mode/html_elixir",
-                  initialValue: "<html><body><h2>Hello Ace!</h2></body></html>"
+                  initialValue: "<html><body><h2>Web registrazione html</h2></body></html>"
                 })
+              ]),
+              m(".inline field", [
+                m(".ui checkbox", [
+                  m("input", {
+                    type: "checkbox",
+                    id: "html-registration-web-include-disclaimer",
+                  }),
+                  m("label", {for: "html-registration-web-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                ])
+              ])
+            ])
+          ]),
+          m(".ui tab segment", {"data-tab": "web-text-registration"}, [
+            m("form.ui form", [
+              m(".field", [
+                m("label", "Oggetto"),
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
               ]),
               m(".field", [
                 m("label", "Body (formato testo)"),
@@ -46,204 +70,220 @@ const deviceContextView = {
                   fontSize: "16px",
                   theme: "ace/theme/twilight",
                   mode: "ace/mode/text",
-                  initialValue: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?"
+                  initialValue: "Web registazione testo"
                 })
               ]),
               m(".inline field", [
                 m(".ui checkbox", [
                   m("input", {
                     type: "checkbox",
-                    id: "web-registration-include-disclaimer",
+                    id: "text-registration-web-include-disclaimer",
+                  }),
+                  m("label", {for: "text-registration-web-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                ])
+              ])
+            ])
+          ])
+        ]),
+        m(".ui tab segment", {"data-tab": "web-confirm-registration"}, [
+          m(".ui pointing secondary confirm-registration-format menu", {
+            oncreate(_) {
+              $('div[data-tab="web"] .confirm-registration-format.menu .item').tab();
+            }
+          }, [
+            m("a.item active", {"data-tab": "web-html-confirm-registration"}, "HTML"),
+            m("a.item", {"data-tab": "web-text-confirm-registration"}, "TESTO")
+          ]),
+          m(".ui tab segment active", {"data-tab": "web-html-confirm-registration"}, [
+            m("form.ui form", [
+              m(".field", [
+                m("label", "Oggetto"),
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
+              ]),
+              m(".field", [
+                m("label", "Body (formato html)"),
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/html_elixir",
+                  initialValue: "<html><body><h2>Web conferma registrazione html</h2></body></html>"
+                })
+              ]),
+              m(".inline field", [
+                m(".ui checkbox", [
+                  m("input", {
+                    type: "checkbox",
+                    id: "web-confirm-registration-include-disclaimer",
                   }),
                   m("label", {for: "web-registration-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
                 ])
               ])
             ])
           ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Conferma Registrazione"
-          ]),
-          m(".content", [
+          m(".ui tab segment", {"data-tab": "web-text-confirm-registration"}, [
             m("form.ui form", [
               m(".field", [
                 m("label", "Oggetto"),
-                m("input", {type: "text", name: "web-registration-confirm-subject", placeholder: "Oggetto della mail"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato html)"),
-                m("textarea", {name: "web-registration-confirm-body-html", placeholder: "Body in formato html"})
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
               ]),
               m(".field", [
                 m("label", "Body (formato testo)"),
-                m("textarea", {name: "web-registration-confirm-body-text", placeholder: "Body in formato testo"})
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/text",
+                  initialValue: "Web conferma registrazione testo"
+                })
               ]),
               m(".inline field", [
                 m(".ui checkbox", [
                   m("input", {
                     type: "checkbox",
-                    id: "web-registration-confirm-include-disclaimer",
+                    id: "web-confirm-registration-include-disclaimer",
                   }),
-                  m("label", {for: "web-registration-confirm-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                  m("label", {for: "web-confirm-registration-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
                 ])
               ])
-            ])
-          ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Reset password"
-          ]),
-          m(".content", [
-            m("form.ui form", [
-              m(".field", [
-                m("label", "Oggetto"),
-                m("input", {type: "text", name: "web-password-reset-subject", placeholder: "Oggetto della mail"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato html)"),
-                m("textarea", {name: "web-password-reset-body-html", placeholder: "Body in formato html"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato testo)"),
-                m("textarea", {name: "web-password-reset-body-text", placeholder: "Body in formato testo"})
-              ]),
-              m(".inline field", [
-                m(".ui checkbox", [
-                  m("input", {
-                    type: "checkbox",
-                    id: "web-password-reset-include-disclaimer",
-                  }),
-                  m("label", {for: "web-password-reset-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
-                ])
-              ])
-            ])
-          ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Disclaimer"
-          ]),
-          m(".content", [
-            m("form.ui form", [
-              m(".field", [
-                m("label", "Disclaimer (formato html)"),
-                m("textarea", {name: "web-password-reset-disclaimer-html", placeholder: "Disclaimer in formato html"})
-              ]),
-              m(".field", [
-                m("label", "Disclaimer (formato testo)"),
-                m("textarea", {name: "web-password-reset-disclaimer-text", placeholder: "Disclaimer in formato testo"})
-              ]),
             ])
           ])
         ])
       ]),
       m(".ui tab segment", {"data-tab": "mobile"}, [
-        m("h4.ui header", "Template email SSO per applicazioni mobile"),
-        m(".ui template styled accordion", {
-          oncreate({dom}) {
-            $(dom).accordion();
+        m(".ui pointing secondary template menu", {
+          oncreate(_) {
+            $('div[data-tab="mobile"] .template.menu .item').tab();
           }
         }, [
-          m(".active title", [
-            m("i.dropdown icon"),
-            "Registrazione"
+          m("a.item active", {"data-tab": "mobile-registration"}, "Registrazione"),
+          m("a.item", {"data-tab": "mobile-confirm-registration"}, "Conferma Registrazione")
+        ]),
+        m(".ui tab segment active", {"data-tab": "mobile-registration"}, [
+          m(".ui pointing secondary registration-format menu", {
+            oncreate(_) {
+              $('div[data-tab="mobile"] .registration-format.menu .item').tab();
+            }
+          }, [
+            m("a.item active", {"data-tab": "mobile-html-registration"}, "HTML"),
+            m("a.item", {"data-tab": "mobile-text-registration"}, "TESTO")
           ]),
-          m(".active content", [
+          m(".ui tab segment active", {"data-tab": "mobile-html-registration"}, [
             m("form.ui form", [
               m(".field", [
                 m("label", "Oggetto"),
-                m("input", {type: "text", name: "mobile-registration-subject", placeholder: "Oggetto della mail"})
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
               ]),
               m(".field", [
                 m("label", "Body (formato html)"),
-                m("textarea", {name: "mobile-registration-body-html", placeholder: "Body in formato html"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato testo)"),
-                m("textarea", {name: "mobile-registration-body-text", placeholder: "Body in formato testo"})
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/html_elixir",
+                  initialValue: "<html><body><h2>Mobile registrazione html</h2></body></html>"
+                })
               ]),
               m(".inline field", [
                 m(".ui checkbox", [
                   m("input", {
                     type: "checkbox",
-                    id: "mobile-registration-include-disclaimer",
+                    id: "html-registration-mobile-include-disclaimer",
+                  }),
+                  m("label", {for: "html-registration-mobile-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                ])
+              ])
+            ])
+          ]),
+          m(".ui tab segment", {"data-tab": "mobile-text-registration"}, [
+            m("form.ui form", [
+              m(".field", [
+                m("label", "Oggetto"),
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
+              ]),
+              m(".field", [
+                m("label", "Body (formato testo)"),
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/text",
+                  initialValue: "Mobile registrazione testo"
+                })
+              ]),
+              m(".inline field", [
+                m(".ui checkbox", [
+                  m("input", {
+                    type: "checkbox",
+                    id: "text-registration-mobile-include-disclaimer",
+                  }),
+                  m("label", {for: "text-registration-mobile-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                ])
+              ])
+            ])
+          ])
+        ]),
+        m(".ui tab segment", {"data-tab": "mobile-confirm-registration"}, [
+          m(".ui pointing secondary confirm-registration-format menu", {
+            oncreate(_) {
+              $('div[data-tab="mobile"] .confirm-registration-format.menu .item').tab();
+            }
+          }, [
+            m("a.item active", {"data-tab": "mobile-html-confirm-registration"}, "HTML"),
+            m("a.item", {"data-tab": "mobile-text-confirm-registration"}, "TESTO")
+          ]),
+          m(".ui tab segment active", {"data-tab": "mobile-html-confirm-registration"}, [
+            m("form.ui form", [
+              m(".field", [
+                m("label", "Oggetto"),
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
+              ]),
+              m(".field", [
+                m("label", "Body (formato html)"),
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/html_elixir",
+                  initialValue: "<html><body><h2>Mobile conferma registrazione html</h2></body></html>"
+                })
+              ]),
+              m(".inline field", [
+                m(".ui checkbox", [
+                  m("input", {
+                    type: "checkbox",
+                    id: "mobile-confirm-registration-include-disclaimer",
                   }),
                   m("label", {for: "mobile-registration-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
                 ])
               ])
             ])
           ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Conferma Registrazione"
-          ]),
-          m(".content", [
+          m(".ui tab segment", {"data-tab": "mobile-text-confirm-registration"}, [
             m("form.ui form", [
               m(".field", [
                 m("label", "Oggetto"),
-                m("input", {type: "text", name: "mobile-registration-confirm-subject", placeholder: "Oggetto della mail"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato html)"),
-                m("textarea", {name: "mobile-registration-confirm-body-html", placeholder: "Body in formato html"})
+                m("input", {type: "text", placeholder: "Oggetto della mail"})
               ]),
               m(".field", [
                 m("label", "Body (formato testo)"),
-                m("textarea", {name: "mobile-registration-confirm-body-text", placeholder: "Body in formato testo"})
+                m(aceEditor, {
+                  className: "html-editor",
+                  fontSize: "16px",
+                  theme: "ace/theme/twilight",
+                  mode: "ace/mode/text",
+                  initialValue: "Mobile conferma registrazione testo"
+                })
               ]),
               m(".inline field", [
                 m(".ui checkbox", [
                   m("input", {
                     type: "checkbox",
-                    id: "mobile-registration-confirm-include-disclaimer",
+                    id: "mobile-confirm-registration-include-disclaimer",
                   }),
-                  m("label", {for: "mobile-registration-confirm-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
+                  m("label", {for: "mobile-confirm-registration-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
                 ])
               ])
-            ])
-          ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Reset password"
-          ]),
-          m(".content", [
-            m("form.ui form", [
-              m(".field", [
-                m("label", "Oggetto"),
-                m("input", {type: "text", name: "mobile-password-reset-subject", placeholder: "Oggetto della mail"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato html)"),
-                m("textarea", {name: "mobile-password-reset-body-html", placeholder: "Body in formato html"})
-              ]),
-              m(".field", [
-                m("label", "Body (formato testo)"),
-                m("textarea", {name: "mobile-password-reset-body-text", placeholder: "Body in formato testo"})
-              ]),
-              m(".inline field", [
-                m(".ui checkbox", [
-                  m("input", {
-                    type: "checkbox",
-                    id: "mobile-password-reset-include-disclaimer",
-                  }),
-                  m("label", {for: "mobile-password-reset-include-disclaimer", style: "cursor: pointer;"}, "Includi Disclaimer")
-                ])
-              ])
-            ])
-          ]),
-          m(".title", [
-            m("i.dropdown icon"),
-            "Disclaimer"
-          ]),
-          m(".content", [
-            m("form.ui form", [
-              m(".field", [
-                m("label", "Disclaimer (formato html)"),
-                m("textarea", {name: "mobile-password-reset-disclaimer-html", placeholder: "Disclaimer in formato html"})
-              ]),
-              m(".field", [
-                m("label", "Disclaimer (formato testo)"),
-                m("textarea", {name: "mobile-password-reset-disclaimer-text", placeholder: "Disclaimer in formato testo"})
-              ]),
             ])
           ])
         ])
