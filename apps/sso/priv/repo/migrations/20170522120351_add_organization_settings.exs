b2af268f8731e6cce10065386db9413c812cb0b8
biospank <fabio.petrucci@gmail.com>
2017-06-05 19:48:07 +0200

    remove index row size 2912 exceeds maximum 2712

--- apps/sso/priv/repo/migrations/20170522120351_add_organization_settings.exs
+++ apps/sso/priv/repo/migrations/20170522120351_add_organization_settings.exs
@@ -5,8 +5,5 @@ defmodule Sso.Repo.Migrations.AddOrganizationSettings do
     alter table(:organizations) do
       add :settings, :map
     end
-
-    create index(:organizations, [:settings])
-
   end
 end
